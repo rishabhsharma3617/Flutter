092f30d3c9dcee590cbc3e48299302c621408d41
Rishabh Sharma <rishabhs@geekyants.com>
2020-10-01 10:58:43 +0530

    Data types added

---
+++ Basics/data_types.dart
@@ -0,0 +1,15 @@
+void main() {
+  int a = 5;
+  double b = 6.8;
+  num c = 6.5;
+  String str = "Hello how are you doing";
+  bool flag = (a == b);
+  List arr = ['hey', 'wht', 'are', 'you', 'doing'];
+  print(a);
+  print(b);
+  print(c);
+  print(str);
+  print(flag);
+  print(arr[0]);
+  print(arr);
+}
